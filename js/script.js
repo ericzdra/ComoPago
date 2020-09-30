7fd949615d342d9fa25a454d810e4a65f45e89b5
ericzdra <eric.zdravlje@gmail.com>
2020-09-30 20:36:24 -0300

    Terminación de Index y ajuste de Script

--- js/script.js
+++ js/script.js
@@ -1,37 +1,38 @@
 
-/*
+
 
 var onePaymentPrice = parseFloat(prompt("Ingresá el precio del producto en 1 pago (pago al contado)"));
 var paymentsQ = parseInt(prompt("Ingresá la cantidad de cuotas que te ofrecen"));
 var paymentsPrice = parseFloat(prompt("Ingresá cuanto te sale cada cuota"));
 var monthlyInflation = parseFloat(prompt("Ingresá la inflación mensual promedio"))
-
-
-
-var inflationAdjustedPayments = inflationAdjusted(paymentsQ, paymentsPrice, monthlyInflation);
-
-var inflationAdjustedTotal = inflationAdjustedPayments.reduce(inflationAdjustedSum);
-
-console.log(inflationAdjustedTotal)
+var myPayment = new Payment(paymentsQ, paymentsPrice, 0 ,monthlyInflation)
+console.log(myPayment)
 
 
 
 
 
-
-function inflationAdjusted(dues, price, inflation) {
-    var adjustedPayments = [0];
-
-    for (var i = 1; i == dues; i++) {
-        adjustedPayments[i] = ((price)/Math.pow((1+(inflation/100)),i));
-
+function Payment(paymentsAmount, paymentsValue, paymentsTotal, inflation) {
+    this.pQ = paymentsAmount
+    this.pV = paymentsValue
+    this.pT = paymentsTotal
+    this.i = inflation
+    this.adjustedPayment = []
+    this.notAdjustedPayment = []
+    this.adjustedPaymentTotal = 0;
+    for (var index = 0; index < this.pQ; index++) {
+        this.notAdjustedPayment.push(this.pV)
+    }
+    for (var index = 0; index < this.notAdjustedPayment.length; index++) {
+        this.adjustedPayment.push(((this.pV) / Math.pow((1 + (this.i / 100)), index)))
     }
 
-    return adjustedPayments
+    this.adjustedPaymentTotal = this.adjustedPayment.reduce(adjustedPaymentSum)
+
+    function adjustedPaymentSum(a, b) {
+        return a+b
+    }
+
 }
 
-function inflationAdjustedSum (a, b) {
-    return a + b;
-}
 
-*/
